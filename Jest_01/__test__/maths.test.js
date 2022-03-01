935ad3d4bf5fe6a7dd50c533340358efb3e307f9
Jorge Calderon <calderon.rjorge@gmail.com>
2022-03-01 13:50:49 -0400

    init jest updt

--- Jest_01/__test__/maths.test.js
+++ Jest_01/__test__/maths.test.js
@@ -2,6 +2,11 @@ import { sumar, multiplicar, restar, dividir } from '../maths.js';
 
 describe('Calculos matematicos ', () => {
   test('Prueba de sumas', () => {
-    expect(sumar(1, 1)).toBe(3);
+    expect(sumar(1, 1)).toBe(2);
   });
+
+  test('Prueba de multiplicar', () => {
+    expect(multiplicar(1, 1)).toBe(2);
+  });
+
 });
